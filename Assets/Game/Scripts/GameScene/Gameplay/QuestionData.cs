222b0b19e1009f151c05b68519d4633d1da9b94a
Patrick Karlo Pamplona <patrick.pam@pocketplaylab.com>
2016-02-25 21:08:21 +0800

    question set and data

---
+++ Assets/Game/Scripts/GameScene/Gameplay/QuestionData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using JsonFx.Json;
+
+[Serializable]
+[JsonName("QuestionData")]
+public class QuestionData
+{
+    public int               id;
+    public QUESTION_CATEGORY category;
+    public string            question;
+    public string            answer;
+}
+
+public enum QUESTION_CATEGORY
+{
+    THREE_LETTERS = 3,
+    FOUR_LETTERS  = 4,
+    FIVE_LETTERS  = 5
+}
---
+++ Assets/Game/Scripts/GameScene/Gameplay/QuestionSet.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using JsonFx.Json;
+
+[Serializable]
+[JsonName("QuestionSet")]
+public class QuestionSet
+{
+    public int               id;
+    public QUESTION_CATEGORY category;
+    public char[]            charactersInChoices;
+    public int[]             questionSet;
+}
