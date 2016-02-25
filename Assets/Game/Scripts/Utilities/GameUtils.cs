e2a0d7ed930190a15863e27120e556d6470417d4
Patrick Karlo Pamplona <patrick.pam@pocketplaylab.com>
2016-02-25 21:05:50 +0800

    game utils script

---
+++ Assets/Game/Scripts/Utilities/GameUtils.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.IO;
+
+public class GameUtils
+{
+    public static string LoadAppFile(string fileName) 
+    {
+        string filePath = "";
+
+        #if UNITY_5
+        if(Application.isEditor || Application.platform == RuntimePlatform.WP8Player || Application.platform == RuntimePlatform.WSAPlayerARM)
+        {
+            filePath = Application.streamingAssetsPath + "/" + fileName;
+            if (File.Exists(filePath)) 
+            {
+                StreamReader r = File.OpenText(filePath);
+
+                if (r != null) 
+                {
+                    string data = r.ReadToEnd();
+                    #if NETFX_CORE
+                        r.Dispose();
+                    #else
+                        r.Close();
+                    #endif
+                    return data;
+                }
+            }
+
+        }
+        #endif
+
+        return "";
+    }
+}
