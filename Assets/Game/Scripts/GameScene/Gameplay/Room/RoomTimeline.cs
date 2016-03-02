9bd434fa84ece5c1d667b257c833f445452b18f1
Patrick Karlo Pamplona <patrick.pam@pocketplaylab.com>
2016-03-02 20:41:26 +0800

    rom timeline

---
+++ Assets/Game/Scripts/GameScene/Gameplay/Room/RoomTimeline.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RoomTimeline : MonoBehaviour
+{
+    [SerializeField] private float roomTimeLimit;
+    private float roomTimer;
+
+    private bool isRoomActive = false;
+    public bool IsRoomActive
+    {
+        get
+        {
+            return this.isActiveAndEnabled;
+        }
+    }
+
+    void Start()
+    {
+        GameplayManager.Instance.CentralizedUpdate += this.RoomUpdate;
+    }
+
+    public void EnterRoom()
+    {
+        this.roomTimer = 0.0f;
+        this.isRoomActive = true;
+    }
+
+    public void ExitRoom()
+    {
+        this.isRoomActive = false;
+    }
+
+    private void RoomUpdate(float deltaTime, float gameRunTime, float timeLimit, float ratioBeforeGameOver)
+    {
+        if(!this.isRoomActive)
+        {
+            return;
+        }
+
+        roomTimer += deltaTime;
+
+        if(roomTimer >= this.roomTimeLimit)
+        {
+            
+        }
+    }
+}
