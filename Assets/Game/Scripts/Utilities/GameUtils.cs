becd0acb3a67461b81fd659c0455a2d1909e8145
Patrick Karlo Pamplona <patrick.pam@pocketplaylab.com>
2016-03-21 22:48:18 +0800

    updated load file

--- Assets/Game/Scripts/Utilities/GameUtils.cs
+++ Assets/Game/Scripts/Utilities/GameUtils.cs
@@ -1,27 +1,84 @@
 ﻿using UnityEngine;
 using System.IO;
+using System;
 
 public class GameUtils
 {
     public static string LoadAppFile(string fileName) 
     {
-        string filePath = "";
+        string filePath="";
 
+        #if UNITY_WEBPLAYER
+        //if playing on webplayer use loaded file cache
+        if(PPLSDKConfig.inst.DebugAssetServer || !Application.isEditor)
+        {
+            if(fileCache.ContainsKey(fileName))
+            {
+                return fileCache[fileName].ToString();
+            }
+            else
+            {
+                if(fileCache.Count == 0)
+                {
+                    Debug.LogError("File cache is empty. PreloadAssetFiles() may have not been called yet.");
+                }
+                
+                return "";
+            }
+        }
+        #endif
+
+        if (Application.platform== RuntimePlatform.IPhonePlayer) 
+        {
+            filePath = Application.dataPath + "/Raw/" + fileName;
+            if (File.Exists(filePath)) 
+            {
+                StreamReader r = File.OpenText(filePath);
+                if (r != null) 
+                {
+                    string data = r.ReadToEnd();
+                    #if NETFX_CORE
+                    r.Dispose();
+                    #else
+                    r.Close();
+                    #endif
+                    return data;
+                }
+            }
+        }
+        else  if(Application.platform == RuntimePlatform.Android)
+        {
+            filePath = "jar:file://" + Application.dataPath + "!/assets/" + fileName;
+            WWW loadFile = new WWW(filePath);
+
+            while(!loadFile.isDone) 
+            {
+
+            }
+
+            if( !string.IsNullOrEmpty(loadFile.text) )
+            {
+                var resultData = loadFile.text;
+                GC.Collect();
+                return resultData;
+            }
+        }
         #if UNITY_5
-        if(Application.isEditor || Application.platform == RuntimePlatform.WP8Player || Application.platform == RuntimePlatform.WSAPlayerARM)
+        else  if(Application.isEditor || Application.platform == RuntimePlatform.WP8Player || Application.platform == RuntimePlatform.WSAPlayerARM)
         {
             filePath = Application.streamingAssetsPath + "/" + fileName;
             if (File.Exists(filePath)) 
             {
+
                 StreamReader r = File.OpenText(filePath);
 
                 if (r != null) 
                 {
                     string data = r.ReadToEnd();
                     #if NETFX_CORE
-                        r.Dispose();
+                    r.Dispose();
                     #else
-                        r.Close();
+                    r.Close();
                     #endif
                     return data;
                 }
