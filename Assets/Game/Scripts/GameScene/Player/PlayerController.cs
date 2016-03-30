2a454df151da62d99a64742a3b74cfd8f14cfbf2
Patrick Karlo Pamplona <patrick.pam@pocketplaylab.com>
2016-03-30 10:47:59 +0800

    added player controller for animation

---
+++ Assets/Game/Scripts/GameScene/Player/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using DG.Tweening;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private const string RUN_ANIM   = "_anim_run";
+    private const string PANIC_ANIM = "_anim_panic";
+
+    [SerializeField] private Animation animation;
+
+    public void Run()
+    {
+        animation.Play(RUN_ANIM);
+    }
+
+    public void Panic()
+    {
+        
+    }
+
+    public void UsePaw(Vector3 target)
+    {
+        
+    }
+}
