8ba0034de03a8333c2b8af4a10541748b80a5ea5
Patrick Karlo Pamplona <patrick.pam@pocketplaylab.com>
2016-03-30 20:53:41 +0800

    info messenger static storage

---
+++ Assets/Game/Scripts/Utilities/InfoMessenger.cs
@@ -0,0 +1,21 @@
+﻿public class InfoMessenger
+{
+    private static bool didPlayerWin = false;
+    public static bool DidPlayerWin
+    {
+        get
+        {
+            return didPlayerWin;
+        }
+    }
+
+    public static void Reset()
+    {
+        didPlayerWin = false;
+    }
+
+    public static void SetPlayerWin(bool win)
+    {
+        didPlayerWin = win;
+    }
+}
