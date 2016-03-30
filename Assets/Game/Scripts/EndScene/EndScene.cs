ca13d0d7d26c40adf0ba5ca724d169cf634b2dea
Patrick Karlo Pamplona <patrick.pam@pocketplaylab.com>
2016-03-30 20:52:46 +0800

    end scene script

---
+++ Assets/Game/Scripts/EndScene/EndScene.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class EndScene : MonoBehaviour
+{
+    [SerializeField] private GameObject winScreen;
+    [SerializeField] private GameObject loseScreen;
+
+	void Start () 
+    {
+        winScreen.SetActive(InfoMessenger.DidPlayerWin);
+        loseScreen.SetActive(!InfoMessenger.DidPlayerWin);
+	}
+
+    public void OnReplay()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+}
