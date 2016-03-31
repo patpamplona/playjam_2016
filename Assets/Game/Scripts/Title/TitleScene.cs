f2c3633ea1ec7b857564f91b91d7455880ae01df
Patrick Karlo Pamplona <patrick.pam@pocketplaylab.com>
2016-03-31 22:47:22 +0800

    title scene script

---
+++ Assets/Game/Scripts/Title/TitleScene.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class TitleScene : MonoBehaviour 
+{
+    public void OnPlayTapped()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+}
