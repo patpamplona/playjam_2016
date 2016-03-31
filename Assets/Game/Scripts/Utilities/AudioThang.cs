8c97bd1290400c6544d9616d117abcb546d343a8
Patrick Karlo Pamplona <patrick.pam@pocketplaylab.com>
2016-04-01 01:22:34 +0800

    added audio thang

---
+++ Assets/Game/Scripts/Utilities/AudioThang.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioThang : MonoBehaviour
+{
+    private static AudioThang _instance = null;
+    public static AudioThang Instance
+    {
+        get
+        {
+            return _instance;
+        }
+    }
+
+    [SerializeField] private AudioSource correct;
+    [SerializeField] private AudioSource incorrect;
+
+    void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+        _instance = this;
+    }
+
+    void OnDestroy()
+    {
+        _instance = null;
+    }
+
+    public void PlayCorrect()
+    {
+        this.incorrect.Stop();
+        this.correct.Play();
+    }
+
+    public void PlayIncorrect()
+    {
+        this.correct.Stop();
+        this.incorrect.Play();
+    }
+}
